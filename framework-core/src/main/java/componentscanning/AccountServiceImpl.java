a6dff330ffbe94337e41a9a539c121d055475dba
Lakshitha Pathirage <npathirage07@gmail.com>
2026-02-19 12:54:05 +0530

    shutdown hook

--- framework-core/src/main/java/componentscanning/AccountServiceImpl.java
+++ framework-core/src/main/java/componentscanning/AccountServiceImpl.java
@@ -1,5 +1,7 @@
 package componentscanning;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +21,23 @@ public class AccountServiceImpl implements AccountService {
     public void transfer() {
         accountRepository.save();
         System.out.println("money transferred!");
+    }
+
+    // this is not a spring specific annotation but from Jakarta
+    // after the IOC container create the service bean and inject dependencies, the method will be executed
+    // theme methods can be invoked with the @Bean as well. But as arguments.
+    // since these are not spring annotations, these are from JSR-250. But spring supports them
+    @PostConstruct
+    private void populateCache() {
+        System.out.println("populating cache!");
+    }
+
+    // this method is executed sooner before the IOC container destroy the service bean
+    // in case under a JVM related sudden termination, this will not be invoked properly
+    // this cannot be used with prototype bean scope [Spring does not manage the full lifecycle of prototype beans,
+    // it only creates them]
+    @PreDestroy
+    private void flushCache() {
+        System.out.println("flushing cache!");
     }
 }
--- framework-core/src/main/java/componentscanning/AppRunner.java
+++ framework-core/src/main/java/componentscanning/AppRunner.java
@@ -7,9 +7,21 @@ public class AppRunner {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // spring application context needs to be closed properly before exiting the application
+        // if not closed properly, sooner the main method done, JVM kill it without properly closing
+        // doing so, the IOC container fails to properly terminates the beans due respect to their life cycles
+        // there are two ways to overcome this. (1) manually close the application context (2) registerShutdownHook
+        // with approach 2, jvm calls the hook if it terminates under healthy conditions and the spring will then
+        // close the application context using close() properly
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         // accessing the service bean from the IOC container
         AccountService accountService = applicationContext.getBean(AccountServiceImpl.class);
         accountService.transfer();
+
+        //manually closing
+        // applicationContext.close();
+
+        // registering the shutdown hook in the jvm
+        applicationContext.registerShutdownHook();
     }
 }
